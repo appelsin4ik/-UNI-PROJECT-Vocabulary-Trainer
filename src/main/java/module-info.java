60109f32f60ee9a7f8c7cce96f3d1e0dc4b1417d
Florian Stemmler <florian.stemmler@outlook.de>
2025-05-07 17:36:54 +0200

    fasterxml.jackson hinzugefügt

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module project.main {
     requires javafx.controls;
     requires javafx.graphics;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
 
     exports project;
 }
--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -10,8 +10,6 @@ import javafx.stage.Stage;
  * Main Application Class
  */
 public class Main extends Application {
-
-
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
