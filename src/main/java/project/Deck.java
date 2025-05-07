8a7e9db4931f26e926ef20dac44d7253b1ca6986
Florian Stemmler <florian.stemmler@outlook.de>
2025-05-07 17:37:37 +0200

    save und load Methoden für Deck implementiert

--- src/main/java/project/Deck.java
+++ src/main/java/project/Deck.java
@@ -1,6 +1,12 @@
 package project;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class Deck {
     private List<Card> cards;
@@ -9,7 +15,45 @@ public class Deck {
     public Deck(String name) {
         this.name = name;
         this.cards = new ArrayList<Card>();
-        // Initialize empty, or populate with cards
+    }
+
+    /**
+     * ObjectMapper instance erstellen
+     * @return jackson ObjectMapper
+     */
+    public static ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        // ObjectMapper für pretty print convertieren
+        //objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        return objectMapper;
+    }
+
+    /**
+     * läd ein neues Dech als Json aus einer Datei
+     * @param filepath Pfad der Datei aus der geladen wird
+     * @return das geladene Deck
+     */
+    public static Deck load(String filepath) throws IOException {
+        // json Datei Daten lesen
+        byte[] jsonData = Files.readAllBytes(Paths.get("employee.txt"));
+
+        // ObjectMapper instance erstellen
+        var mapper = Deck.getObjectMapper();
+        // json string mit ObjectMapper zu object convertieren
+        return mapper.readValue(jsonData, Deck.class);
+    }
+
+    /**
+     * dieses Deck als Json in einer Datei speichern
+     * @param filepath Pfad zu dieser Datei
+     */
+    public void save(String filepath) throws IOException {
+        // ObjectMapper instance erstellen
+        var mapper = Deck.getObjectMapper();
+
+        // dieses Deck in Datei schreiben
+        var file = new File(filepath);
+        mapper.writeValue(file, this);
     }
 
     public void AddCard(String vocabulary, String translation) {
